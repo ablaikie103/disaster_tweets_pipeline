eb6e2428920586f6f672935d91fa898dd7389125
Andrew Blaikie <ablaikie103@gmail.com>
2020-05-20 15:27:48 -0700

    Modified flask app

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from wtforms import Form, TextAreaField, validators
+import pickle
+import numpy as np
+"""
+clf = pickle.load(open('classifier/trained_classifier.pkl'))
 
+def label_tweet(tweet):
+    y = clf.predict(tweet)
+    return y
+"""
 app = Flask(__name__)
+
+class HelloForm(Form):
+    sayhello = TextAreaField('', [validators.DataRequired()])
 
 @app.route('/')
 def index():
-    return render_template('disaster_tweet_app.html')
+    form = HelloForm(request.form)
+    return render_template('disaster_tweet_app.html', form=form)
+
+@app.route('/hello', methods=['POST'])
+def hello():
+    form = HelloForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = request.form['sayhello']
+        return render_template('hello.html', name=name)
+    return render_template('disaster_tweet_app.html', form=form)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
